44f6aa942b4c7cf55efd43ef9d6894d15b36a89b
t1jp <103856744+t1jp@users.noreply.github.com>
2022-09-13 17:38:06 -0600

    Disenio Agregado a algunas Vistas
    Co-Authored-By: Ghostman <55126792+GhostManV@users.noreply.github.com>

--- Panaderia/Models/UsuariosViewModel.cs
+++ Panaderia/Models/UsuariosViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,28 @@ namespace Panaderia.Models
     public class UsuariosViewModel
     {
         public int id_usuario { get; set; }
+        
+        [Display(Name = "Nombre")]
         public string nombre { get; set; }
+        
+        [Display(Name = "Apellidos")]
         public string apellido { get; set; }
+        
+        [Display(Name = "Direccion")]
         public string direccion { get; set; }
+        
+        [Display(Name = "Email")]
         public string email { get; set; }
+        
+        [Display(Name ="Usuario")]
         public string users { get; set; }
+        
+        [Display(Name = "Contraseña")]
         public string pass { get; set; }
+
+        
         public int id_rol { get; set; }
+        [Display(Name = "Rol")]
         public string rol { get; set; }
     }
 }
